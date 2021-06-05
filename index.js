94c5920b019389e6c1d5d95bf3d4c9eeb9d1d334
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-05 10:52:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 function findMatching(drivers, name){
   let newArray = [];
-  newArray = drivers.filter(driver=>driver.toLowerCase()==name.toLowerCase());
+  newArray = drivers.filter(driver=> driver.toLowerCase() == name.toLowerCase());
   return newArray;
 }
 
-function fuzzyMatch (drivers, text){
+function fuzzyMatch (drivers, name){
   let newArray = [];
   newArray = drivers.filter(driver=>text.includes(driver[0]));
   return newArray;
 }
 
-function matchName (drivers, text){
+function matchName (drivers, name){
   let newArray = [];
   newArray = drivers.filter(driver=>driver.name==text);
   return newArray;
