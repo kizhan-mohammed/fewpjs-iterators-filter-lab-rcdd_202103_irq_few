74d75b9ac231f05f7d514195a801b894e8c2add4
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-05 10:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+
+function findMatching (drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
